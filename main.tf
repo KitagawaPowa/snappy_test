ce574ee5b2baff74b96fcd47dd05cf6987896115
KitagawaPowa <kitagawapowa@gmail.com>
2021-02-08 01:39:45 +0000

    Update main.tf
    updated ami to match with the right region

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-005c06c6de69aee84"
   instance_type = "t2.micro"
   tags = {
     Name = "terraform-example"
