d719c8bc90507349f09f6749afa3b037f7837858
KitagawaPowa <kitagawapowa@gmail.com>
2021-02-09 18:46:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-005c06c6de69aee84"
   instance_type = "t2.micro"
+  count = 2
   
  tags = {
    Name = "terraform-example"
