0ee062791bd79e1a87a51129c7110724635d0ad5
KitagawaPowa <kitagawapowa@gmail.com>
2021-02-11 17:54:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-005c06c6de69aee84"
   instance_type = "t2.micro"
-  count = 1
+  count = 2
   
  tags = {
    Name = "terraform-vcs_driven"
+   CostCenter = "Cloud Ops"
   }
 }
 
